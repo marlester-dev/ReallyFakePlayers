bbb59b11219a40d4d0db0cea92b1d647d86b1466
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-03-10 13:33:53 +0100

    Adapt ReallyFakePlayersLoader.java to Google Java Style Guide

--- src/main/java/me/marlester/rfp/ReallyFakePlayersLoader.java
+++ src/main/java/me/marlester/rfp/ReallyFakePlayersLoader.java
@@ -8,6 +8,10 @@ import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The loader class for this plugin.
+ * Its main goal now is to load up libraries of the plugin.
+ */
 public class ReallyFakePlayersLoader implements PluginLoader {
 
   @Override
@@ -15,18 +19,28 @@ public class ReallyFakePlayersLoader implements PluginLoader {
     MavenLibraryResolver resolver = new MavenLibraryResolver();
 
     // Add repositories
-    resolver.addRepository(new RemoteRepository.Builder("central", "default", "https://repo1.maven.org/maven2").build());
-    resolver.addRepository(new RemoteRepository.Builder("opencollab", "default", "https://repo.opencollab.dev/maven-releases").build());
-    resolver.addRepository(new RemoteRepository.Builder("jitpack", "default", "https://jitpack.io").build());
+    resolver.addRepository(new RemoteRepository.Builder("central", "default",
+        "https://repo1.maven.org/maven2").build());
+    resolver.addRepository(new RemoteRepository.Builder("opencollab", "default",
+        "https://repo.opencollab.dev/maven-releases").build());
+    resolver.addRepository(new RemoteRepository.Builder("jitpack", "default",
+        "https://jitpack.io").build());
 
     // Add dependencies
-    resolver.addDependency(new Dependency(new DefaultArtifact("com.google.inject:guice:7.0.0"), null));
-    resolver.addDependency(new Dependency(new DefaultArtifact("com.google.inject.extensions:guice-assistedinject:7.0.0"), null));
-    resolver.addDependency(new Dependency(new DefaultArtifact("org.javassist:javassist:3.30.2-GA"), null));
-    resolver.addDependency(new Dependency(new DefaultArtifact("net.bytebuddy:byte-buddy-agent:1.14.12"), null));
-    resolver.addDependency(new Dependency(new DefaultArtifact("com.github.steveice10:mcprotocollib:1.20.4-1"), null));
-    resolver.addDependency(new Dependency(new DefaultArtifact("dev.dejvokep:boosted-yaml-spigot:1.4"), null));
-    resolver.addDependency(new Dependency(new DefaultArtifact("xyz.jpenilla:reflection-remapper:0.1.0"), null));
+    resolver.addDependency(new Dependency(new DefaultArtifact(
+        "com.google.inject:guice:7.0.0"), null));
+    resolver.addDependency(new Dependency(new DefaultArtifact(
+        "com.google.inject.extensions:guice-assistedinject:7.0.0"), null));
+    resolver.addDependency(new Dependency(new DefaultArtifact(
+        "org.javassist:javassist:3.30.2-GA"), null));
+    resolver.addDependency(new Dependency(new DefaultArtifact(
+        "net.bytebuddy:byte-buddy-agent:1.14.12"), null));
+    resolver.addDependency(new Dependency(new DefaultArtifact(
+        "com.github.steveice10:mcprotocollib:1.20.4-1"), null));
+    resolver.addDependency(new Dependency(new DefaultArtifact(
+        "dev.dejvokep:boosted-yaml-spigot:1.4"), null));
+    resolver.addDependency(new Dependency(new DefaultArtifact(
+        "xyz.jpenilla:reflection-remapper:0.1.0"), null));
 
     classpathBuilder.addLibrary(resolver);
   }
